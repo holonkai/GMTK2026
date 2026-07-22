6713a95fa6cce98cd6dc7a22672d0e278576c364
AidenGutierrez <113128555+AidenGutierrez@users.noreply.github.com>
2026-07-22 14:08:55 -0700

    Jump sfx added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sfx: AudioStreamPlayer2D = $"../JumpSFX"
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
@@ -22,6 +25,7 @@ func _physics_process(delta: float) -> void:
 	# Jump
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y =JUMP_VELOCITY
+		jump_sfx.play(0.0)
 		
 	# Player direction
 	var direction := Input.get_axis("Left", "Right")
