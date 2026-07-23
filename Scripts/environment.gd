4641f1e6b630e2104768e4f58da3f19c5d9bedbd
rennic <rennictaylor@gmail.com>
2026-07-23 16:48:20 -0700

    visible countdown/semi fixed the moving blocks

--- Scripts/environment.gd
+++ Scripts/environment.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var layers: Array[TileMapLayer] = [$LayerA, $LayerB, $LayerC]
 @onready var timer: Timer = $DisappearTimer
+@onready var countdown: Label = $countdown
 
 var current_index: int = 1
 var is_timer_active: bool = false
@@ -10,25 +11,34 @@ func _ready() -> void:
 	# Show initial active layer
 	_update_active_layer()
 	timer.timeout.connect(_on_timer_timeout)
+	trigger_block_disappear()
+
+# Countdown display
+func _process(_delta: float) -> void:
+	print("is_timer_active=", is_timer_active, " stopped=", timer.is_stopped())
+	if not timer.is_stopped():
+		countdown.text = "The Footholds will switch in %.1f" % timer.time_left
+	else:
+		countdown.text = ""
 
 func trigger_block_disappear() -> void:
 	if not is_timer_active:
 		is_timer_active = true
-		timer.start(5.0)
+		timer.start(10.0)
 
 func _on_timer_timeout() -> void:
 	current_index = (current_index + 1) % layers.size()
 	print(current_index)
 	# Want to update the layer mask first maybe an animation
 	_update_active_layer()
-	timer.start(5.0)
+	timer.start(10.0)
 
 func _update_active_layer() -> void:
 	for i in range(layers.size()):
 		var layer = layers[i]
 		var is_current = (i == current_index)
 		
-		#set the new layer to visible
+		# Set the new layer to visible
 		layer.visible = is_current
 		layer.collision_enabled = is_current
 
