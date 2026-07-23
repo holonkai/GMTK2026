69d07cd1b9b86500435712850792203d5bd347f3
rennic <rennictaylor@gmail.com>
2026-07-23 16:12:24 -0700

    update switching blocks

--- Scripts/environment.gd
+++ Scripts/environment.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var layers: Array[TileMapLayer] = [$LayerA, $LayerB, $LayerC]
 @onready var timer: Timer = $DisappearTimer
 
-var current_index: int = 0
+var current_index: int = 1
 var is_timer_active: bool = false
 
 func _ready() -> void:
@@ -14,14 +14,14 @@ func _ready() -> void:
 func trigger_block_disappear() -> void:
 	if not is_timer_active:
 		is_timer_active = true
-	
+		timer.start(5.0)
 
 func _on_timer_timeout() -> void:
 	current_index = (current_index + 1) % layers.size()
+	print(current_index)
 	# Want to update the layer mask first maybe an animation
 	_update_active_layer()
-	
-	timer.start(5.0) # Timer
+	timer.start(5.0)
 
 func _update_active_layer() -> void:
 	for i in range(layers.size()):
@@ -30,7 +30,8 @@ func _update_active_layer() -> void:
 		
 		#set the new layer to visible
 		layer.visible = is_current
-		
+		layer.collision_enabled = is_current
+
 		layer.set_process_mode(
 			PROCESS_MODE_INHERIT if is_current else PROCESS_MODE_DISABLED
 		)
